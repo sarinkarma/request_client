3814ad530fb262ddb4c1d144b3cbaffbfd6ae7c1
Sarin Karmacharya <sarinkarma@gmail.com>
2021-11-08 10:17:48 +0545

    add test function

--- lib/src/function.dart
+++ lib/src/function.dart
@@ -1,11 +1,11 @@
 class Add {
-  final int value1;
-  final int value2;
+  final int? value1;
+  final int? value2;
 
   Add(this.value1, this.value2);
 
   int addValue(){
-    int result = value1 + value2;
+    int result = value1! + value2!;
     return result;
   }
 
